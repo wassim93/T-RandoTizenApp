0cc1a36c3ba069ab2c83bcf4ed035c66f6215284
medaymenTN <mohamedaymen.ourabi11@gmail.com>
2018-11-05 23:24:09 +0100

    complete post detail + posr CRUD

--- js/blog/blog.js
+++ js/blog/blog.js
@@ -1,6 +1,7 @@
   
 var loadData = () => {
     var LikedButtonColor = " "
+    var upsuppbutt = "hidden"
     var xhr = new XMLHttpRequest()
     xhr.open('GET', 'http://localhost:8000/getAllPosts', false)
 
@@ -15,18 +16,29 @@ var loadData = () => {
 
                  LikedButtonColor = "red"
               }
-              
+              var checku = await checkUser(x.id, localStorage.getItem("access_token"))
+              if(JSON.parse(checku).response==="true"){
+                       upsuppbutt = "visible"
+                    
+              }
 
-                $('#myList').append(`<div class="type-post">
+                $('#myList').append(
+                    `<div class="type-post">
+                            <div style="visbility:${upsuppbutt}" align="right">
+                            <i class="fas fa-cog"  onclick="edit(${x.id},event)">
+                            </i>
+                            <i class="fas fa-trash"  onclick="supprimer(${x.id})">
+                            </i> 
+                            </div>
 							<div class="entry-cover">
 								<a href="#"><img src="http://localhost:8000/image/${x.image_path}" alt="Blog" /></a>
 							</div>
 							<div class="entry-meta">
 								<div class="post-like" >
-									<a href="#"><i style="color:${LikedButtonColor}"  class="fa fa-heart-o"></i> ${x.likes} L</a>
+									<a href="#"><i style="color:${LikedButtonColor}"  class="fa fa-heart"></i> ${x.likes} L</a>
 								</div>
 								<div class="post-comment">
-									<a href="#"><i  class="fa fa-comment-o"></i>${x.comments} C</a>
+									<a href="#"><i  class="fa fa-comment"></i>${x.comments} C</a>
 								</div>
 								<div class="byline">
 									Posted by <a href="#">${x.user._fisrt_name}</a>
@@ -75,9 +87,58 @@ var checkLike = (idpost, token) => {
     })
 
 }
+var checkUser = (idpost, token) => {
+    return new Promise((resolve, reject) => {
 
+        var xhr = new XMLHttpRequest()
+        xhr.open('POST', 'http://localhost:8000/checkPostuser', true)
+
+        xhr.onload = () => {
+            if (xhr.readyState === 4) {
+                resolve(xhr.responseText)
+
+            }
+        }
+        xhr.onerror = function () {
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send(JSON.stringify({
+            idpost: idpost,
+            token: token
+        }))
+
+
+    })
+
+}
 var  ShowDetail = (id,event)=>{
 event.preventDefault()
 localStorage.setItem("idpostdetail",id)
 window.location = "blog-single.html"
+}
+var edit = (id,event)=>{
+    event.preventDefault()
+    localStorage.setItem("idpostdetail",id)
+    window.location = "EditPost.html"
+
+
+}
+
+var AddPost = ()=>{
+    window.location = "addPost.html"
+}
+
+var supprimer = (id)=>{
+
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET', `http://localhost:8000/Removepost/${id}`, false)
+
+    xhr.onreadystatechange = () => {
+        window.location = "blog.html"
+    }
+    xhr.send()
+
 }
